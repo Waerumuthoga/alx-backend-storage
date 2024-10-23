622dba0052decf2c9faac46eec278c7df4bd93c3
Waerumuthoga <waerumuthoga@gmail.com>
2024-10-23 19:31:35 +0000

    task 102

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""Python script for mongoDB
+"""
+from pymongo import MongoClient
+
+
+def print_request_logs(nginx_collection):
+    """Prints detailss about Nginx request logs.
+    """
+    print('{} logs'.format(nginx_collection.count_documents({})))
+    print('Methods:')
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    for method in methods:
+        req_count = len(list(nginx_collection.find({'method': method})))
+        print('\tmethod {}: {}'.format(method, req_count))
+    status_checks_count = len(list(
+        nginx_collection.find({'method': 'GET', 'path': '/status'})
+    ))
+    print('{} status check'.format(status_checks_count))
+
+
+def print_top_10_ips(server_collection):
+    """Prints details about the top 10 HTTP IPs in a collection.
+    """
+    print('IPs:')
+    request_logs = server_collection.aggregate(
+        [
+            {
+                '$group': {'_id': "$ip", 'totalRequests': {'$sum': 1}}
+            },
+            {
+                '$sort': {'totalRequests': -1}
+            },
+            {
+                '$limit': 10
+            },
+        ]
+    )
+    for request_log in request_logs:
+        ip = request_log['_id']
+        ip_requests_count = request_log['totalRequests']
+        print('\t{}: {}'.format(ip, ip_requests_count))
+
+
+def run():
+    """Provides some stats about Nginx logs stored in MongoDB.
+    """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    print_request_logs(client.logs.nginx)
+    print_top_10_ips(client.logs.nginx)
+
+
+if __name__ == '__main__':
+    run()
