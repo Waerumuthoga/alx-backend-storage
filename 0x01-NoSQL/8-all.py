ba5cd4d0d8f4184ff94b77349c9b86b6b7531122
Waerumuthoga <waerumuthoga@gmail.com>
2024-10-23 19:16:27 +0000

    task 8

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Python script for mongoDB.
+"""
+
+
+def list_all(mongo_collection):
+    """Function that lists all documents in a collection"""
+    return [doc for doc in mongo_collection.find()]
